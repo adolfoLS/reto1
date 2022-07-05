0ec4f4a57cdca8fe97f1e107b5844c1d94b7273d
Adolfo Lozada Serena <adolfo.lozada@globant.com>
2022-07-05 14:56:32 -0500

    Commit 1

---
+++ reto1/reto1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reto1
+//
+//  Created by Adolfo Lozada Serena on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
